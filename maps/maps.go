ef186a24100535f78960a96acfce3436c4af3fc8
Nikita Babichev <nick.babichev@gmail.com>
2026-08-11 14:34:38 +0300

    Maps topic done

--- maps/maps.go
+++ maps/maps.go
@@ -3,8 +3,40 @@ package main
 import "fmt"
 
 func main() {
+
+	// Initialize map with make()
 	m := make(map[string]int)
 
+	// Set some data in map using key-value approach
 	m["route"] = 66
+	m["corner"] = 1
 	fmt.Println(m)
+
+	i := m["route"]
+	j := m["corner"]
+	k := m["hornet"]
+
+	// Getting length of map
+	n := len(m)
+
+	// Removed item from map
+	delete(m, "route")
+	fmt.Println(i, j, k, n)
+	fmt.Println(m)
+
+	// Using Comma-Ok Idiom
+	i, ok := m["route"]
+
+	if ok {
+		fmt.Println(i)
+	} else { // It will be else
+		fmt.Println("Route is not found")
+	}
+
+	m["route"] = 666
+
+	// Iteration over the contents of a map
+	for k, v := range m {
+		fmt.Println("Key: ", k, "Value: ", v)
+	}
 }
